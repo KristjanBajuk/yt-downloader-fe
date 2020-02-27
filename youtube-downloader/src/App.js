878dd319fdde9ee5bc6a74b21d662fdf358aa6d4
KristjanBajuk <kristjan.bajuk@ineor.si>
2020-02-27 10:58:24 +0100

    add logo, url input, submit button

--- youtube-downloader/src/App.js
+++ youtube-downloader/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
+import logo from "./assets/logo.png";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import SendIcon from '@material-ui/icons/Send';
+
 
 function App() {
   return (
-    <div>
-        Youtube Downloader
-    </div>
+      <div className="container">
+          <div className="mainContent">
+            <img className="logo" src={logo}/>
+            <form noValidate autoComplete="off">
+                  <TextField className="urlInput" id="standard-basic" label="URL" />
+                  <Button
+                      variant="contained"
+                      color="primary"
+                      size="large"
+                      startIcon={<SendIcon />}
+                  >
+                      Send
+                  </Button>
+            </form>
+          </div>
+      </div>
   );
 }
 
