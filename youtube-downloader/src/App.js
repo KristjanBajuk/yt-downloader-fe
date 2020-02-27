4197e96af997425fc65f4ba82ea9f3bd7dcc0692
KristjanBajuk <kristjan.bajuk@ineor.si>
2020-02-27 14:33:02 +0100

    send url to server

--- youtube-downloader/src/App.js
+++ youtube-downloader/src/App.js
@@ -6,14 +6,31 @@ import Button from '@material-ui/core/Button';
 import SendIcon from '@material-ui/icons/Send';
 
 
-function App() {
+const App = () => {
+    
+    const sendRequest = async (event) => {
+        event.preventDefault();
+      const response = await fetch("http://localhost:9999/youtube-download",{
+          method: 'POST', // or 'PUT'
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({url: event.target.url.value}),
+      });
+     
+      console.log("STATUS: ", response);    
+      
+      
+    };
+    
   return (
       <div className="container">
           <div className="mainContent">
-            <img className="logo" src={logo}/>
-            <form noValidate autoComplete="off">
-                  <TextField className="urlInput" id="standard-basic" label="URL" />
+            <img alt="logo" className="logo" src={logo}/>
+            <form noValidate autoComplete="off" onSubmit={sendRequest}>
+                  <TextField className="urlInput" name="url" id="standard-basic" label="URL" />
                   <Button
+                      type="submit"
                       variant="contained"
                       color="primary"
                       size="large"
